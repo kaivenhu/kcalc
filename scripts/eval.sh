43b2d60e998e87c0d3768b1b68a905e0be78a382
Kai-Wen Hu <kainbd0609@gmail.com>
2020-07-02 21:45:55 +0800

    Add unit-test on test script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -4,26 +4,43 @@
 command -v bc >/dev/null 2>&1 || \
     { echo >&2 "This script requires 'bc' but it's not installed.  Aborting.";
       exit 1; }
+COLOR_RESET='\e[0m'
+COLOR_GREEN='\e[0;32m';
+COLOR_RED='\e[0;31m';
 
 # Transfer the self-defined representation to real number
 fromfixed() {
     local ret=$1
+    local ans=${2}
     local NAN_INT=31
     local INF_INT=47
+    local is_eq=0
+    local scale=""
 
     num=$(($ret >> 4))
     frac=$(($ret & 15))
     neg=$((($frac & 8) >> 3))
 
-    [[ $neg -eq 1 ]] && frac=$((-((~$frac & 15) + 1)))
+    [[ $neg -eq 1 ]] && frac=$((-((~$frac & 15) + 1))) && scale="scale=$((-(${frac})));"
 
     if [ "$ret" -eq "$NAN_INT" ]
     then
-        echo "NAN_INT"
+        ret="NAN_INT"
+        [[ "${ret}" == "${ans}" ]] && is_eq="1"
     elif [ "$ret" -eq "$INF_INT" ]
     then
-        echo "INF_INT"
+        ret="INF_INT"
+        [[ "${ret}" == "${ans}" ]] && is_eq="1"
     else
-        echo "$num*(10^$frac)" | bc -l
+        ret="`echo \"${scale} $num*(10^$frac)\" | bc -l`"
+        is_eq="`echo \"${scale} ($num*(10^$frac) == (${ans}))\" | bc -l`"
+        ans="`echo \"${scale} ${ans}\" | bc -l`"
+    fi
+
+    if [ "${is_eq}" != "1" ]
+    then
+        echo -e "${COLOR_RED} Failed: ${COLOR_RESET} ret = ${ret} ans = ${ans}"
+    else
+        echo -e "${COLOR_GREEN} PASS: ${COLOR_RESET} ret = ${ret}"
     fi
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -8,9 +8,16 @@ source scripts/eval.sh
 
 test_op() {
     local expression=$1 
+    local ans=${2}
     echo "Testing " ${expression} "..."
     echo -ne ${expression}'\0' > $CALC_DEV
-    fromfixed $(cat $CALC_DEV)
+
+    if [ -z ${ans} ]
+    then
+        ans=${expression}
+    fi
+
+    fromfixed $(cat $CALC_DEV) ${ans}
 }
 
 if [ "$EUID" -eq 0 ]
@@ -41,50 +48,49 @@ test_op '42/6'
 test_op '1/3'
 test_op '1/3*6+2/4'
 test_op '(1/3)+(2/3)'
-test_op '(2145%31)+23'
-test_op '0/0' # should be NAN_INT
+test_op '(2145%31)+23' '29'
+test_op '0/0' 'NAN_INT' # should be NAN_INT
 
 # binary
-test_op '(3%0)|0' # should be 0
-test_op '1+2<<3' # should be (1 + 2) << 3 = 24
-test_op '123&42' # should be 42
-test_op '123^42' # should be 81
+test_op '(3%0)|0' '0' # should be 0
+test_op '1+2<<3' '24' # should be (1 + 2) << 3 = 24
+test_op '123&42' '42' # should be 42
+test_op '123^42' '81' # should be 81
 
 # parens
 test_op '(((3)))*(1+(2))' # should be 9
 
 # assign
-test_op 'x=5, x=(x!=0)' # should be 1
-test_op 'x=5, x = x+1' # should be 6
+test_op 'x=5, x=(x!=0)' '1' # should be 1
+test_op 'x=5, x = x+1' '6' # should be 6
 
 # fancy variable name
-test_op 'six=6, seven=7, six*seven' # should be 42
-test_op '小熊=6, 維尼=7, 小熊*維尼' # should be 42
-test_op 'τ=1.618, 3*τ' # should be 3 * 1.618 = 4.854
-test_op '$(τ, 1.618), 3*τ()' # shold be 3 * 1.618 = 4.854
+test_op 'six=6, seven=7, six*seven' '42' # should be 42
+test_op '小熊=6, 維尼=7, 小熊*維尼' '42' # should be 42
+test_op 'τ=1.618, 3*τ' '4.854' # should be 3 * 1.618 = 4.854
+test_op '$(τ, 1.618), 3*τ()' '4.854' # shold be 3 * 1.618 = 4.854
 
 # functions
-test_op '$(zero), zero()' # should be 0
-test_op '$(one, 1), one()+one(1)+one(1, 2, 4)' # should be 3
-test_op '$(number, 1), $(number, 2+3), number()' # should be 5
+test_op '$(zero), zero()' '0' # should be 0
+test_op '$(one, 1), one()+one(1)+one(1, 2, 4)' '3' # should be 3
+test_op '$(number, 1), $(number, 2+3), number()' '5' # should be 5
 
 # pre-defined function
-test_op 'nop()'
-test_op 'fib(10+3*10)'
+test_op 'nop()' '-0.1'
+test_op 'fib(10+3*10)' '0'
 
 # Livepatch
 sudo insmod $LIVEPATCH_CALC_MOD
 sleep 1
 echo "livepatch was applied"
-test_op 'nop()'
-test_op 'fib(10+3*10)'
-test_op 'fib(10+3*10+1)'
+test_op 'nop()' '0'
+test_op 'fib(10+3*10)' '102334155'
+test_op 'fib(10+3*10+1)' '0'
 dmesg | tail -n 6
 echo "Disabling livepatch..."
 sudo sh -c "echo 0 > /sys/kernel/livepatch/livepatch_calc/enabled"
-sleep 2
+sleep 5
 sudo rmmod livepatch-calc
-
 sudo rmmod calc
 
 # epilogue
